e82192f6c2c9e1506f2877101030f171c91913bf
Baspel <brianovkolp@gmail.com>
2024-06-17 14:10:50 +0200

    feat: button for communicating

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -1,6 +1,34 @@
 <script>
     import { zaznamy, selectedZaznam } from '../../lib/store.js';
+    import fs from 'fs';
     
+    async function doPost(zaznam) {
+        const res = await fetch('table', {
+			method: 'POST',
+            body: JSON.stringify(zaznam),
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+		})
+
+        console.log(res);
+
+        if (res.ok) {
+            const blob = await res.blob();
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'report.docx');
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+        } else {
+            console.error('Server response was not ok.');
+        }
+         
+    }
+
     function editRow(zaznam) {
         selectedZaznam.set(zaznam);
         console.log($selectedZaznam);
@@ -34,7 +62,7 @@
                         <td>
                             <button>Protokol</button>
                             <button>Protokol</button>
-                            <button>Protokol</button>
+                            <button  on:click={() => doPost(zaznam)}>Protokol</button>
                         </td>
                         <td>
                             <input type="checkbox" />
